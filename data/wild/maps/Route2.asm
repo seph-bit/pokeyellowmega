cce7b77e1911ade27c17e10583559906c052c5e7
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 16:54:18 -0600

    Update Route2.asm

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -1,15 +1,15 @@
 Route2WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, RATTATA
-	db  3, PIDGEY
-	db  4, RATTATA
-	db  4, NIDORAN_M
-	db  4, NIDORAN_F
-	db  5, PIDGEY
-	db  6, NIDORAN_M
-	db  6, NIDORAN_F
+	db  4, PIDGEY
+	db  5, ODDISH
+	db  4, CATERPIE
+	db  6, ODDISH
+	db  5, CATERPIE
+	db  6, PIDGEY
+	db  4, WEEDLE
 	db  7, PIDGEY
-	db  7, PIDGEY
+	db  5, WEEDLE
+	db  7, WEEDLE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
