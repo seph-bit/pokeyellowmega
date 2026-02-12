0ec33485a4a6953990ce5ca68459523c35ba0986
seph-bit <seraphim.beatle@gmail.com>
2026-02-12 16:55:59 -0600

    Update ViridianForest.asm

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,14 +1,14 @@
 ViridianForestWildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, CATERPIE
-	db  4, METAPOD
 	db  4, CATERPIE
-	db  5, CATERPIE
 	db  4, PIDGEY
-	db  6, PIDGEY
-	db  6, CATERPIE
-	db  6, METAPOD
-	db  8, PIDGEY
+	db  4, WEEDLE
+	db  7, CATERPIE
+	db  4, METAPOD
+	db  7, PIDGEY
+	db  6, WEEDLE
+	db  6, PARAS
+	db  8, PARAS
 	db  9, PIDGEOTTO
 	end_grass_wildmons
 
